0099b3f0be8b7c5e2c88e7556ecc8ed5656aa20e
lukesmith123 <lukesmith@bukowski.net>
2014-11-23 15:41:31 +0000

    maps

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
- var map;
-    //$(document).ready(function(){
+function initialise() {
+         
+    	var myLatlng = new google.maps.LatLng(31.555197,-91.403171);  // Add the coordinates
+    	var mapOptions = {
+        zoom: 15, // The initial zoom level when your map loads (0-20)
+        center: myLatlng, // Centre the Map to our coordinates variable
+        mapTypeId: google.maps.MapTypeId.ROADMAP, // Set the type of Map
+		disableDefaultUI: true,
+      	}
+    	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions); // Render our map within the empty div
+         
+		var image = new google.maps.MarkerImage("http://www.google.com/mapfiles/marker.png", null, null, null, new google.maps.Size(20,26)); // Create a variable for our marker image.
+             
+		var markerLatLng = new google.maps.LatLng(31.559404, -91.408971);	 
+			 
+		var marker = new google.maps.Marker({ // Set the marker
+    	position: markerLatLng, // Position marker to coordinates
+    	icon:image, //use our image as the marker
+    	map: map, // assign the market to our map variable
+    	title: 'Click to visit our company on Google Places' // Marker ALT Text 
+		});
+		
+		var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Rosalie Mansion:</h1>'+
+      '<div id="bodyContent">'+
+      '<p>This historic pre-civil war mansion was built in 1823. Rosalie Mansion is owned and operated by the Daughters of the American Revolution. During the Civil War it served as Union Headquarters for the Natchez area. Enjoy the lovely gardens of this National Historic Landmark.</p>'+
+      '</div>'+
+      '</div>';
 
-      map = new GMaps({
-        div: '#map',
-        zoom: 16,
-        lat: -12.043333,
-        lng: -77.028333,
-        click: function(e){
-          alert('click');
-        },
-        dragend: function(e){
-          alert('dragend');
-        }
-      });
-    //});
+  	  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  	  });
+
+	 google.maps.event.addListener(marker, 'click', function() {
+     infowindow.open(map,marker);
+  	 });
+}
+google.maps.event.addDomListener(window, 'load', initialise); // Execute our 'initialise' function once the page has loaded. 
+
