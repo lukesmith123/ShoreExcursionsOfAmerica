a92ec6e4bea80e55d4a7bc99c09750d6b72c6619
lukesmith123 <lukesmith@bukowski.net>
2014-11-23 17:21:15 +0000

    appls

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ function initialise() {
 		var contentString = '<div id="content">'+
       '<div id="siteNotice">'+
       '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Rosalie Mansion:</h1>'+
+      '<h3 id="firstHeading" class="firstHeading">Rosalie Mansion:</h3>'+
       '<div id="bodyContent">'+
       '<p>This historic pre-civil war mansion was built in 1823. Rosalie Mansion is owned and operated by the Daughters of the American Revolution. During the Civil War it served as Union Headquarters for the Natchez area. Enjoy the lovely gardens of this National Historic Landmark.</p>'+
       '</div>'+
@@ -37,6 +37,25 @@ function initialise() {
 	 google.maps.event.addListener(marker, 'click', function() {
      infowindow.open(map,marker);
   	 });
+	 
+	var flightPlanCoordinates = [
+    new google.maps.LatLng(31.560192, -91.403269),
+    new google.maps.LatLng(31.562587, -91.401016),
+    new google.maps.LatLng(31.562011, -91.400147),
+    new google.maps.LatLng(31.560430, -91.401617),
+	new google.maps.LatLng(31.559177, -91.399782),
+	new google.maps.LatLng(31.558317, -91.400519),
+	new google.maps.LatLng(31.560195, -91.403266),
+  ];
+  var flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: '#f68b1f',
+    strokeOpacity: 1.0,
+    strokeWeight: 4
+  });
+
+  flightPath.setMap(map);
 }
 google.maps.event.addDomListener(window, 'load', initialise); // Execute our 'initialise' function once the page has loaded. 
 
